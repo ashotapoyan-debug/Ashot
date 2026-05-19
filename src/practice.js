a048d2380b28e1b017dfee22fed9f7b5509a2fd4
Student <ashot.apoyan@tumo.org>
2026-05-19 20:28:39 +0400

    Javascript

--- src/practice.js
+++ src/practice.js
@@ -24,3 +24,13 @@ const issPosition = {
 console.log(issPosition.latitude);
 console.log(issPosition.longitude);
 console.log(issPosition);
+
+const asteroids = [
+    { name: "2024 AB1",diameter: 120,hazardous: false },
+    { name: "2024 CD2",diameter: 45,hazardous: true },
+    { name: "2024 EF3",diameter: 890,hazardous: false },
+    { name: "2024 GH4", diameter: 23,hazardous: true }
+    ]
+const names = asteroids.map(person => person.name)
+console.log(names);
+const issOnly = crew.filter(person =>)
