a06302ff493350407325462b3c1fbc4eb22034f0
Student <ashot.apoyan@tumo.org>
2026-05-21 19:07:28 +0400

    Vercel css

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
+import './App.css'
 const n = 123
 export default function App() {
   return (
-    <div className="Ashot">
+    <div className="dashboard">
       <h1>Ashot Apoyan</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic accusamus officiis aut inventore sit, suscipit consequuntur reiciendis error maiores excepturi eos, laudantium repellat incidunt obcaecati itaque, animi ratione! Vitae, officiis!</p>
-    <ISSCard />
+    <ISSCard/>
     <A/>
     </div>
   )
 }
 function ISSCard() {
   return (
-  <div className="Ashot">
+  <div className="Card">
     <h2>ISS </h2>
     <p>{n}</p>
   </div>
@@ -19,7 +20,7 @@ function ISSCard() {
 }
 function A() {
   return(
-    <div className="Ashot">
+    <div className="Card">
       <h3>abcd</h3>
     </div>
   )
